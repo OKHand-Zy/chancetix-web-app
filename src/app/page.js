d086d9e4a80a8702b15ae515b94d519f1cb5a325
OKH@nd <OKHand.ZiYu@gmail.com>
2024-01-14 22:47:40 +0800

    image OK

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,12 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <div>test</div>
-      
+      <Image
+                src='/images/activity_img/cat.jpg'
+                width={500}
+                height={500}
+                alt='I am B'
+      />
     </main>
   )
 }
