098302d1c28b7d952ab4d2f86f241a408c18cc96
OKHand-Zy <OKHand.ziyu@gmail.com>
2024-01-30 17:23:28 +0800

    Login_lagcy-complite

--- src/components/Login/LoginButton.jsx
+++ src/components/Login/LoginButton.jsx
@@ -4,23 +4,42 @@ import CreateWind from './CreateWind';
 import LoginWind from './LoginWind';
 
 export default function LoginButton() {
-    const [isPopupOpen, setIsPopupOpen] = useState(false);
+// LoginPopup
+    const [LoginPopupOpen, setLoginPopupOpen] = useState(false);
     const [welcomMessage, setWelcomMessage] = useState('');
-
-    const togglePopup = (account) => {
-        setIsPopupOpen(!isPopupOpen);
-        if (account) {
+    const LoginData = (account) => {
+        setLoginPopupOpen(!LoginPopupOpen);
+        if (account && account === 'CreateAccount') {
+            // 如果 account 是 'CreateAccount'，触发 CreatePopup()
+            CreatePopup();
+        } else if (account) {
             setWelcomMessage(`Welcome ${account}`);
         } else {
-            setWelcomMessage(''); // 如果沒有帳號資訊，清空歡迎訊息
+            setWelcomMessage(''); // 如果没有帐號資訊，清空歡迎訊息
         }
     };
-    
+    const LogOut = () => {  // 簡易 Logout 須改 
+        setWelcomMessage('');
+    }
+// CreatePopup
+    const [CreatePopupOpen, setCreatePopupOpen] = useState(false);
+    const CreatePopup = () => {
+        setCreatePopupOpen(!CreatePopupOpen);
+    };
+
     return (
-        <div>
-            <button onClick={() => togglePopup()}>Login Account</button>
-            {isPopupOpen && <LoginWind togglePopup={togglePopup} />}
-            <div>{welcomMessage}</div>
+        <div className='flex justify-between space-x-10 items-center'>
+            <div>
+                {LoginPopupOpen && <LoginWind togglePopup={LoginData} />}
+                {welcomMessage
+                    ? <button onClick={() => LogOut()}>{welcomMessage}</button>  // 登入後顯示
+                    : <button onClick={() => LoginData()}>Login Account</button> // 登入前顯示
+                }
+            </div>
+            <div>
+                {CreatePopupOpen && <CreateWind togglePopup={CreatePopup} />}
+                {!welcomMessage && <button onClick={() => CreatePopup()}>Create Account</button>}
+            </div>
         </div>
     )
 }
