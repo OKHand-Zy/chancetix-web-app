ffc4eb1fc0235cd6695a0a4ef8ff799237ec24cd
OKH@nd <OKHand.ZiYu@gmail.com>
2024-02-20 22:50:35 +0800

    LoginHome

---
+++ src/app/(Auth)/LoginHome/page.tsx
@@ -0,0 +1,26 @@
+import LoginButton from '@/src/components/Auth/login-button'
+import React from 'react'
+
+type Props = {}
+
+export default function page({}: Props) {
+  return (
+    <main className='flex h-full flex-col items-center justify-center bg-sky-500'>
+        <div className="space-y-6 text-center">
+            <h1 className='text-6xl font-semibold text-white drop-shadow-md'>
+                🔐 Auth               
+            </h1>
+            <p className='text-white text-lg'>
+                simple auth service    
+            </p>
+            <div>
+                <LoginButton >
+                    <button className="bg-green-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                            Sign in
+                    </button>
+                </LoginButton>
+            </div>
+        </div>
+    </main>    
+  )
+}
