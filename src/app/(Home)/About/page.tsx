2b58eb8da77891af4fbd3167f01755c8e1cf8444
OKH@nd <OKHand.ZiYu@gmail.com>
2024-03-04 23:48:47 +0800

    About content

--- src/app/(Home)/About/page.tsx
+++ src/app/(Home)/About/page.tsx
@@ -16,145 +16,45 @@ function aboutpage({}: Props) {
     <ErrorBoundary>
       <div className='p-20 flex flex-col items-center justify-center text-center '>
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          The Joke Tax Chronicles
+          關於 ChanceTix
         </h1>
         <p className="leading-7 [&:not(:first-child)]:mt-6">
-          Once upon a time, in a far-off land, there was a very lazy king who
-          spent all day lounging on his throne. One day, his advisors came to him
-          with a problem: the kingdom was running out of money.
+        我們是一個全新的售票平台，致力於為消費者提供更公平、更便利的購票體驗。
         </p>
-          
+        <p className="leading-7 [&:not(:first-child)]:mt-6">
+        這是由一位熱愛看演唱會的工程師打造的，他深知眾多歌迷在購票過程中所遭遇到的種種困擾，因此決心創建一個能夠有效打擊黃牛行為的平台。
+        </p>
+        <p>總的來說，我們的平台致力於為消費者打造一個公平、安全、便利的購票環境，並與廠商和歌手合作，建立起良好的合作關係，共同推動表演產業的發展。</p>  
         <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-          The King's Plan
+          為什麼要選擇我們？
         </h2>
-        <p className="leading-7 [&:not(:first-child)]:mt-6">
-          The king thought long and hard, and finally came up with{" "}
-          <a
-            href="#"
-            className="font-medium text-primary underline underline-offset-4"
-          >
-            a brilliant plan
-          </a>
-          : he would tax the jokes in the kingdom.
-        </p>
-        <blockquote className="mt-6 border-l-2 pl-6 italic">
-          "After all," he said, "everyone enjoys a good joke, so it's only fair
-          that they should pay for the privilege."
-        </blockquote>
-        
-        <h3 className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">
-          The Joke Tax
-        </h3>
-        <p className="leading-7 [&:not(:first-child)]:mt-6">
-          The king's subjects were not amused. They grumbled and complained, but
-          the king was firm:
-        </p>
-        <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
-          <li>1st level of puns: 5 gold coins</li>
-          <li>2nd level of jokes: 10 gold coins</li>
-          <li>3rd level of one-liners : 20 gold coins</li>
+        <ul className="my-6 ml-6 list-disc [&>li]:mt-2 text-start">
+          <li><b>多種售票方式：</b>我們提供多種售票方式供廠商選擇，包括 抽選制度、實名制購票制度、隨機題目購票以及與一般購票。這樣不僅能夠提供更多選擇給消費者，也能有效防止黃牛的囤票行為。</li>
+          <li><b>官方換票系統：</b>我們的官方換票系統能夠有效防止消費者受到黃牛的詐騙。消費者可以放心使用我們的系統進行票卷交換，保障交易安全。</li>
+          <li><b>實體票卷與電子票卷系統：</b>我們提供廠商選擇的實體票卷與電子票卷系統，以滿足不同消費者的需求和喜好。</li>
+          <li><b>會員優先購票制度：</b>我們未來將盡力爭取與歌手和廠商的 Fans Club 建立一個會員優先購票制度。這樣能夠讓真正的粉絲在第一時間購得票券，同時也能夠促進良好的購票粉絲關係，提升消費者滿意度。</li>
         </ul>
-        <p className="leading-7 [&:not(:first-child)]:mt-6">
-          As a result, people stopped telling jokes, and the kingdom fell into a
-          gloom. But there was one person who refused to let the king's
-          foolishness get him down: a court jester named Jokester.
-        </p>
-        
-        <h3 className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">
-          Jokester's Revolt
-        </h3>
-        <p className="leading-7 [&:not(:first-child)]:mt-6">
-          Jokester began sneaking into the castle in the middle of the night and
-          leaving jokes all over the place: under the king's pillow, in his soup,
-          even in the royal toilet. The king was furious, but he couldn't seem to
-          stop Jokester.
-        </p>
-        <p className="leading-7 [&:not(:first-child)]:mt-6">
-          And then, one day, the people of the kingdom discovered that the jokes
-          left by Jokester were so funny that they couldn't help but laugh. And
-          once they started laughing, they couldn't stop.
-        </p>
-        
-        <h3 className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">
-          The People's Rebellion
-        </h3>
-        <p className="leading-7 [&:not(:first-child)]:mt-6">
-          The people of the kingdom, feeling uplifted by the laughter, started to
-          tell jokes and puns again, and soon the entire kingdom was in on the
-          joke.
-        </p>
-        
-        <div className="my-6 w-full overflow-y-auto">
-          <table className="w-full">
-            <thead>
-              <tr className="m-0 border-t p-0 even:bg-muted">
-                <th className="border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right">
-                  King's Treasury
-                </th>
-                <th className="border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right">
-                  People's happiness
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="m-0 border-t p-0 even:bg-muted">
-                <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
-                  Empty
-                </td>
-                <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
-                  Overflowing
-                </td>
-              </tr>
-              <tr className="m-0 border-t p-0 even:bg-muted">
-                <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
-                  Modest
-                </td>
-                <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
-                  Satisfied
-                </td>
-              </tr>
-              <tr className="m-0 border-t p-0 even:bg-muted">
-                <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
-                  Full
-                </td>
-                <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
-                  Ecstatic
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <p className="leading-7 [&:not(:first-child)]:mt-6">
-          The king, seeing how much happier his subjects were, realized the error
-          of his ways and repealed the joke tax. Jokester was declared a hero, and
-          the kingdom lived happily ever after.
-        </p>
-        <p className="leading-7 [&:not(:first-child)]:mt-6">
-          The moral of the story is: never underestimate the power of a good laugh
-          and always be careful of bad ideas.
-        </p>
         
         <Accordion type="multiple" className="w-8/12">
           
           <AccordionItem value="item-1">
-            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionTrigger>如何聯繫我們?</AccordionTrigger>
             <AccordionContent>
-              Yes. It adheres to the WAI-ARIA design pattern.
+              ChanceTix.service@gmail.com
             </AccordionContent>
           </AccordionItem>
 
           <AccordionItem value="item-2">
-            <AccordionTrigger>Is it styled?</AccordionTrigger>
+            <AccordionTrigger>服務何時上線?</AccordionTrigger>
             <AccordionContent>
-              Yes. It comes with default styles that matches the other
-              components&apos; aesthetic.
+              預計2024年6月底上線
             </AccordionContent>
           </AccordionItem>
-          
+
           <AccordionItem value="item-3">
-            <AccordionTrigger>Is it animated?</AccordionTrigger>
+            <AccordionTrigger>製作人?</AccordionTrigger>
             <AccordionContent>
-              Yes. It's animated by default, but you can disable it if you prefer.
+              OKH@nd
             </AccordionContent>
           </AccordionItem>
           
