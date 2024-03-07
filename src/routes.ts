c506f68801d64ff00d971f22332a63c364544967
OKH@nd <OKHand.ZiYu@gmail.com>
2024-03-08 00:25:13 +0800

    Auth.js Login

---
+++ src/routes.ts
@@ -0,0 +1,31 @@
+/*
+    An array of public routes that are accessible to the public
+    These routes do not require authentication
+    @type {string[]}
+*/
+export const publicRoutes = [
+    "/"
+];
+
+/*
+    An array of public routes that are use for authentication
+    These routes are will require logged in users to /settings
+    @type {string[]}
+*/
+export const authRoutes = [
+    "/auth/login",
+    "/auth/register",
+];
+
+/*
+    The prefix for API authentication routes
+    Route that starts with this prefix prefix are used for API authentication purposes
+    @type {string}
+*/
+export const apiAuthPrefix = "/api/auth";
+
+/*
+    The default redirect path after logging in
+    @type {string}
+*/
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
