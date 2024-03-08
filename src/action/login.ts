b7eee57dd9fc08ddd61c4bbe817bf5609c46c562
OKHand-Zy <OKHand.ziyu@gmail.com>
2024-03-08 17:27:03 +0800

    2024/03/08-Office

--- src/action/login.ts
+++ src/action/login.ts
@@ -8,14 +8,18 @@ import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { AuthError } from "next-auth";
 
 
-export const login = async (values: z.infer<typeof LoginSchema>) => {
+export const login = async (
+    values: z.infer<typeof LoginSchema>,
+) => {
     console.log(values);
     const validatedFields = LoginSchema.safeParse(values);
 
     if (!validatedFields.success) {
         return { error: "Invalid fields!" };
-        };
+    }
     
+    // To DO Email verification retrun success message
+
     const { email, password } = validatedFields.data; 
     try {
         await signIn("credentials", { 
--- src/auth.config.ts
+++ src/auth.config.ts
@@ -2,7 +2,7 @@ import type { NextAuthConfig } from 'next-auth'
 import Credentials from 'next-auth/providers/credentials'
 import Google from "next-auth/providers/google";
 import GitHub from "next-auth/providers/github";
-
+import Line from "next-auth/providers/line";
 import bcrypt from 'bcryptjs';
 
 import { LoginSchema } from '@/schemas';
@@ -17,16 +17,30 @@ export default {
         if (validatedFields.success) {
           const { email, password } = validatedFields.data;
           const user = await getUserByEmail(email);
-          
+          // 確認 User 跟 User Password 是否存在
           if (!user || !user.password) {
             return null;
           }
-          
+          // 輸入 Password 加密 跟 DB 裡的 Password 比較
           const passwordsMatch = await bcrypt.compare(password, user.password);
           if (passwordsMatch) return user;
         }
         
         return null;
       }
-  }), GitHub, Google ]
+  }), 
+    GitHub({ // change to line
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }), 
+    Line({
+      clientId: process.env.LINE_ID,
+      clientSecret: process.env.LINE_SECRET,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }), 
+  ],
+
 } satisfies NextAuthConfig
