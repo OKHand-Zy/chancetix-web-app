b3b204927ce6f7837b859cba27acc050df5079bb
OKHand-Zy <OKHand.ziyu@gmail.com>
2024-03-14 16:48:01 +0800

    switchtixpage-sample

--- src/components/home/SwitchTix/tixpoolfrom.tsx
+++ src/components/home/SwitchTix/tixpoolfrom.tsx
@@ -1,9 +1,44 @@
 
+import { Payment, columns } from "./columns"
+import { DataTable } from "./data-table"
 
-export default function TixpoolFrom() {
-    return (
-        <div>
-            TixpoolFrom
-        </div>
-    )
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "123",
+      amount: 100,
+      status: "pending",
+      email: "B@example.com",
+    },
+    {
+      id: "456",
+      amount: 200,
+      status: "pending",
+      email: "F@example.com",
+    },
+    {
+      id: "728",
+      amount: 500,
+      status: "pending",
+      email: "A@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 900,
+      status: "pending",
+      email: "K@example.com",
+    },
+  ]
+}
+
+export default async function TixpoolFrom() {
+  const data = await getData()
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+    
+  )
 }
--- src/routes.ts
+++ src/routes.ts
@@ -9,6 +9,7 @@ export const publicRoutes = [
     "/Activity",
     "/FAQ",
     "/News",
+    "/SwitchTix",
 ];
 
 /*
