e772496f619b4a6209f3312d62437cc7eb91e05c
OKH@nd <OKHand.ZiYu@gmail.com>
2024-04-03 16:30:57 +0800

    admin-from

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const session = await auth();
+
   return (
     <SessionProvider session={session}>
       <html lang="en">
