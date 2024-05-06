109309453d08e12e7f8a733f02151d113bccc3b4
OKHand-Zy <OKHand.ziyu@gmail.com>
2024-05-06 17:33:30 +0800

    newCardslide Complet

--- src/components/home/NewCarouselide/carouselslide.tsx
+++ src/components/home/NewCarouselide/carouselslide.tsx
@@ -1,4 +1,3 @@
-"use client";
 import React from 'react'
 import EmblaCarousel from './EmblaCarousel'
 import { EmblaOptionsType } from 'embla-carousel'
@@ -10,17 +9,47 @@ const SLIDE_COUNT = 5
 const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 
 export default function NewCarouselSlide() {
-  const images = [
-    '/images/activity_img/avenue.jpg',
-    '/images/activity_img/boat.jpg',
-    '/images/activity_img/Null.png',
-    '/images/activity_img/cat.jpg',
-    '/images/activity_img/rocks.jpg',
-  ];
+  const AcSlide = [
+    {
+      images: '/images/activity_img/Null.png',
+      ArtName: 'Art1',
+      AcName: 'Ac1',
+      url: '/Ac1',
+      badges: ['Now','VIP'],
+    },
+    {
+      images: '/images/activity_img/Null.png',
+      ArtName: 'Art2',
+      AcName: 'Ac2',
+      url: '/Ac2',
+      badges: ['Now'],
+    },
+    {
+      images: '/images/activity_img/Null.png',
+      ArtName: 'Art3',
+      AcName: 'Ac3',
+      url: '/Ac3',
+      badges: ['Now','VIP'],
+    },
+    {
+      images: '/images/activity_img/Null.png',
+      ArtName: 'Art4',
+      AcName: 'Ac4',
+      url: '/Ac4',
+      badges: ['VIP'],
+    },
+    {
+      images: '/images/activity_img/Null.png',
+      ArtName: 'Art5',
+      AcName: 'Ac5',
+      url: '/Ac5',
+      badges: [],
+    }
+  ]
 
   return (
     <>
-      <EmblaCarousel slides={SLIDES} options={OPTIONS} images={images} />
+      <EmblaCarousel slides={SLIDES} options={OPTIONS} AcSlide={AcSlide} />
     </>
   )
 }
