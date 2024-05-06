f4480dcc7a3e17300d7b50c5f83a108d1c2f7945
OKHand-Zy <OKHand.ziyu@gmail.com>
2024-05-06 17:33:30 +0800

    newCardslide Complet

--- src/components/home/NewCarouselide/EmblaCarousel.tsx
+++ src/components/home/NewCarouselide/EmblaCarousel.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { useCallback, useEffect, useRef } from 'react'
 import {
   EmblaCarouselType,
@@ -12,20 +13,38 @@ import {
 } from './EmblaCarouselArrowButtons'
 import { DotButton, useDotButton } from './EmblaCarouselDotButton'
 import Image from 'next/image'
+import Link from 'next/link';
+import {
+  Card,
+  CardHeader, 
+  CardTitle,
+  CardContent,
+  CardFooter,
+  CardDescription,
+} from '@/components/ui/Shadcn/card';
+import { Badge } from '@/components/ui/Shadcn/badge';
 
 const TWEEN_FACTOR_BASE = 0.84
 
 const numberWithinRange = (number: number, min: number, max: number): number =>
   Math.min(Math.max(number, min), max)
 
+interface AcSlide {
+  images: string;
+  ArtName: string;
+  AcName: string;
+  url: string;
+  badges: string[];
+}
+
 type PropType = {
   slides: number[]
-  images: string[]; // Add images prop
+  AcSlide: AcSlide[]
   options?: EmblaOptionsType
 }
 
 const EmblaCarousel: React.FC<PropType> = (props) => {
-  const { slides, options, images } = props
+  const { slides, options, AcSlide } = props
   const [emblaRef, emblaApi] = useEmblaCarousel(options)
   const tweenFactor = useRef(0)
 
@@ -94,20 +113,42 @@ const EmblaCarousel: React.FC<PropType> = (props) => {
       .on('scroll', tweenOpacity)
   }, [emblaApi, tweenOpacity])
 
-
   return (
     <div className="embla">
       <div className="embla__viewport" ref={emblaRef}>
         <div className="embla__container">
           {slides.map((index, slideIndex) => (
             <div className="embla__slide" key={index}>
-              <Image
-                className="embla__slide__img"
-                src={images[slideIndex]}
-                alt="Your alt text"
-                width={512}
-                height={288}
-              />
+              <Card>
+                <CardHeader className="p-4">
+                  <Image
+                      className="embla__slide__img"
+                      src={AcSlide[slideIndex].images}
+                      alt="Your alt text"
+                      width={512}
+                      height={288}
+                    />
+                </CardHeader>
+                <CardContent className="aspect-banner">
+                  <CardTitle className="text-xl font-semibold">
+                    {AcSlide[slideIndex].ArtName}
+                  </CardTitle>
+                  <CardDescription className="text-gray-600 text-xl">
+                      <Link href={AcSlide[slideIndex].url}>
+                        {AcSlide[slideIndex].AcName}
+                      </Link> 
+                  </CardDescription>
+                </CardContent>
+                <CardFooter className='flex space-x-2 p-6 bg-gray-100 rounded'>
+                  {AcSlide[slideIndex].badges.map((badge_tag, index) => (
+                      <span key={index}>
+                        <Badge>
+                          {badge_tag}
+                        </Badge>
+                      </span>
+                  ))}
+                </CardFooter>
+              </Card>
             </div>
           ))}
         </div>
@@ -131,9 +172,8 @@ const EmblaCarousel: React.FC<PropType> = (props) => {
         <div className="embla__buttons">
           <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
         </div>
-
-        
       </div>
+
     </div>
   )
 }
