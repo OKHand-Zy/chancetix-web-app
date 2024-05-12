8989d4c9a4b0d14c09c5b761d89ea0f429cbe61d
OKH@nd <OKHand.ZiYu@gmail.com>
2024-05-12 23:59:45 +0800

    zustand
    use zustand for pr

---
+++ src/store/CountStore.js
@@ -0,0 +1,17 @@
+import {create} from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware'
+
+const useStore = create(
+  persist(
+    (set, get) => ({
+      bears: 0,
+      addABear: () => set({ bears: get().bears + 1 }),
+    }),
+    {
+      name: 'food-storage', // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
+    },
+  ),
+)
+
+export default useStore;
