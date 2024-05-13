73c9a38e2f085e43bbf71274bfbd87cfffe20067
OKHand-Zy <OKHand.ziyu@gmail.com>
2024-05-13 17:43:37 +0800

    office-2024/05/13

--- src/app/Test/page2/page.tsx
+++ src/app/Test/page2/page.tsx
@@ -6,18 +6,35 @@ import {
   CardContent,
 } from "@/components/ui/Shadcn/card";
 import { Button } from "@/components/ui/Shadcn/button";
-
+import Link from 'next/link';
 import React from 'react';
 import CountStore from '@/store/CountStore'
 
 function CountShow() {
   const count = CountStore((state) => state.bears)
-  return <h2>Count: {count}</h2>
+  return (
+    <h2>
+      Count: {count}
+    </h2>
+  )
 }
 
 function CountIncre() {
   const increment = CountStore((state) => state.addABear)
-  return <button onClick={increment}>Increment</button>
+  return (
+    <Button onClick={increment}>
+      Increment
+    </Button>
+  )
+}
+
+function CountReset() {
+  const reset = CountStore((state) => state.restBears)
+  return (
+    <Button onClick={reset}>
+      Reset
+    </Button>
+  )
 }
 
 const BPage = () => {
@@ -28,7 +45,20 @@ const BPage = () => {
     <div>
       <h1>Page B</h1>
       <CountShow />
-      <CountIncre />
+      <div className="flex col gap-2">
+        <CountIncre />
+        <CountReset />
+        <Button asChild variant="outline">
+          <Link href="/Test/page1">
+            Go To PageA
+          </Link>
+        </Button>
+        <Button 
+          onClick={() => {CountStore.persist.clearStorage()}}
+        >
+          Clear Store
+        </Button>
+      </div>
     </div>
   );
 };
