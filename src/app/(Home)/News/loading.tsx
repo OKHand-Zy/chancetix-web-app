8a4a35298e3323a692ac3128827d8fc0ac5b57c0
OKHand-Zy <OKHand.ziyu@gmail.com>
2024-05-27 10:25:01 +0800

    filter all loading animate

--- src/app/(Home)/News/loading.tsx
+++ src/app/(Home)/News/loading.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { FadeLoader } from 'react-spinners';
 
 const Loading = () => {
-    return (
-        <div className='flex justify-center'>
-            <h1>Loading.....</h1>
-        </div>
-    );
+  return (
+    <div className='flex justify-center text-center items-center'>
+      <div>
+        <h1>Loading.....</h1>
+        <FadeLoader color="#10B981" />
+      </div>
+    </div>
+  );
 }
 
 export default Loading;
