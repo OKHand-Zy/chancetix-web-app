3b4535666d898f9b2341a6e6c8dbf253914e0780
OKHand-Zy <OKHand.ziyu@gmail.com>
2024-06-07 15:21:36 +0800

    add init zustand store

--- src/app/(Home)/Activity/sellticket/Activity_2/NormalTicket/page.tsx
+++ src/app/(Home)/Activity/sellticket/Activity_2/NormalTicket/page.tsx
@@ -1,7 +1,8 @@
+"use client";
 import { SellFrom } from "./_components/sell-from";
 
 function Activity_2_Normal() {
-  
+
   const volunteerList: { label: string; value: string; }[] = [
     { label: "Tier1", value: "Normal1" },
     { label: "Tier2", value: "Normal2" },
